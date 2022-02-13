68ffef737755bb2bac5518ac30cd47e90e6116b5
matthewbryantdonovan <matthewbryantdonovan@gmail.com>
2022-02-13 00:04:18 -0500

    starting js lessons

---
+++ JS/01_variables.js
@@ -0,0 +1,20 @@
+var myString = "Has letters and numbers 545465464564!"
+var myInt = 5
+var myArray = [1,2,"three", "4", 5]
+var myObject = {
+    lessons: ["scratch", "text", "css", "priority", "spacing"],
+    lessonsLearned: 5,
+    presentationName: "Full Stack Risking",
+    participants: [
+        {
+            name: "Matthew",
+            age: "34",
+            likes: "Long walks on the beach"
+        },
+        {
+            name: "Joe",
+            age: "?",
+            likes: "Coffee"
+        }
+    ]
+}
