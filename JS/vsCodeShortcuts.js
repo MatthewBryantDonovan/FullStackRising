eaaee7c9b7d7b73a84e44c9a5726e8bcc84039aa
matthewbryantdonovan <matthewbryantdonovan@gmail.com>
2022-03-01 22:20:47 -0500

    updates to html and shortcut file made

---
+++ JS/vsCodeShortcuts.js
@@ -0,0 +1,53 @@
+// 1)
+// Click anywhere in this line and press [CTRL + /]
+
+// 2)
+/*  
+    Highlight the line above, this line, and the line below and press [ALT + SHIFT + A]
+ */
+
+// 3)
+// Press [CTL + SHIFT + K] while the cursor is on this line, oh no!
+// Yay!
+
+// 4)
+// 2
+// 1 - press [ALT + Up arrow] when the cursor is on this line
+// 4 - press [ALT + Down arrow] when the cursor is on this line
+// 3
+
+// 5)
+// SAME_WORD <-- double click "SAME_WORD" and press [CTRL + D] 4 times
+// SAME_WORD
+// SAME_WORD
+
+// 6)
+// Put your cursor before the 'F' in the line below and hold [SHIFT] and press [CTRL + Right arrow]
+// Full_Left
+
+// 7)
+// Put your cursor after the 't' in the line below and hold [SHIFT] and press [CTRL + Left arrow]
+// Full_Right
+
+// 8)
+// On the line below put your curson before the M in "Matt" and press [CTRL + ALT + Down arrow] 2 times
+// Matt likes cake
+// Tom enjoys nap time
+// Joe knows it's 5 o' clock somewhere
+// Now that we have a few cursors up at the same time from step 7 lets do the directions from step 6: hold [SHIFT] and press [CTRL + Right arrow]
+// Then press [CTRL + C]
+
+// 9)
+// After completing step 8 - with the cursor in the empty line below press [CTRL + V]
+
+
+// 10)
+// Lets combine some more. Put the curson at the end of the next line then press [CTRL + ALT + Down arrow] 2 times, then press [CTRL + V]
+// 
+// 
+// 
+
+// 11)
+// Double Trouble - while the cursor is on this line press [ALT + SHIT + Down arrow], then hold [SHIFT] and press [up arrow], then hold [ALT] and press [Down arrow]
+// Double Trouble - while the cursor is on this line press [ALT + SHIT + Down arrow], then hold [SHIFT] and press [up arrow], then hold [ALT] and press [Down arrow]
+// ||||||||||||||
